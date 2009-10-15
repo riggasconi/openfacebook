5a8dd3343d5e60e92bd8660f5ee6c546d0640dee
Sebastiano Scròfina <riggasconi@kaaaki.com>
2009-10-15 15:36:25 +0100

    pickler

---
+++ features/1493379.feature
@@ -0,0 +1,2 @@
+@http://www.pivotaltracker.com/story/show/1493379
+Feature: user can test openfacebook
--- features/step_definitions/get_friends_steps.rb
+++ features/step_definitions/get_friends_steps.rb
@@ -1,2 +1,14 @@
 # author: Sebastiano Scròfina (riggasconi@kaaaki.com)
 # this code is released under the Ruby License
+
+Given /^the scene Default Configuration$/ do
+  pending
+end
+
+When /^I fill in "([^\"]*)"$/ do |arg1|
+  pending
+end
+
+Then /^I should get 8 random friends$/ do
+  pending
+end
